be9a16d3fc16364977da6c9f21b34e0d206d72be
190031003 <190031003@kluniversity.in>
2025-11-17 21:38:27 +0530

    Created basic maven project

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,10 @@
+package com.example;
+
+/**
+ * Main application class
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello I am your Copilot Agent");
+    }
+}
